1a9b441383b2ee9086509c22c92aad71423223aa
Wizi8914 <loick.konieczny@gmail.com>
2021-11-27 19:39:35 +0100

    add resources

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include "main.hpp"
 
 using namespace sf;
+using namespace std;
 
 // Prototypes des fonctions 
 void InputHandler(Event event, RenderWindow& window);
+void loadfont();
+
+Font font;
 
 int main()
 {
     RenderWindow window(VideoMode(WIN_WIDTH, WIN_HEIGHT, 32), "Firstgame");
     window.setVerticalSyncEnabled(true);
+
+    loadfont();
+    Text txt;
+    txt.setFont(font);
+    txt.setString("Hello World !");
+    txt.setCharacterSize(20);
+    txt.setFillColor(Color::Magenta);
+
 
     while (window.isOpen())
     {
@@ -17,6 +29,14 @@ int main()
         {
             InputHandler(event, window);
         }
+
+        window.clear(Color::Black);
+
+        window.draw(txt);
+
+        window.display();
+
+
     }
 
     return 0;
@@ -27,5 +47,13 @@ void InputHandler(Event event, RenderWindow &window)
     if (event.type == Event::Closed)
     {
         window.close();
+    }
+}
+
+void loadfont()
+{
+    if (!font.loadFromFile("resources/roboto.ttf"))
+    {
+        cout << "font can be loaded";
     }
 }
