6d408066a3322acb389c594326e6c988297a60e7
Wizi8914 <loick.konieczny@gmail.com>
2021-11-29 00:02:57 +0100

    add first sprite

--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@
 RenderWindow window;
 Input input;
 
+Texture heroTexture;
+Sprite heroSprite;
+
 int main()
 {
     
     window.create(VideoMode(WIN_WIDTH, WIN_HEIGHT, 32), "Firstgame", Style::Default);
     window.setVerticalSyncEnabled(true);
+
+    if (!heroTexture.loadFromFile("resources/image/hero_sheet.png"))
+    {
+        cout << "Erreur lors du chargement de la texture";
+    }
+
+    heroSprite.setTexture(heroTexture);
+    heroSprite.setTextureRect(IntRect(32, 0, 32, 32));
 
     while (window.isOpen())
     {
@@ -18,8 +29,8 @@ int main()
 
         }
         Checkbtn();
-
         window.clear(Color::Black);
+        window.draw(heroSprite);
         window.display();
 
 
